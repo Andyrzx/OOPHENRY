e8c88cd123f70a8581684c54357f38e17cfbd573
Andyrzx <andyrizz777@gmail.com>
2025-10-02 17:21:10 -0500

    Add files via upload

---
+++ Corte1/PracticasIniciales/Practica2/CalculadoraBasica/OperacionesArirtmeticas.py
@@ -0,0 +1,11 @@
+//completar con el resto de operaciones basicas
+import ClaseOperaciones
+
+obj = ClaseOperaciones.OperacionesBasicas()
+
+obj.setVal(20)
+obj.setVal2(11)
+
+obj.sumar(obj.getVal(),obj.getVal2())
+
+print(obj.getR())
